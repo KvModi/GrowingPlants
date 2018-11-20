4cf6f8f3743cfb550b710d81f7f417c5cf28e3dc
krishnamodi <krishnamodi@10.110.110.94>
2018-11-19 20:59:28 -0500

    before ui

---
+++ PlantProject/src/edu/neu/csye6200/bg/BGRule.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.neu.csye6200.bg;
+
+/**
+ * @author krishnamodi
+ * @NUID 001447420
+ */
+public class BGRule { // Biological Growth
+	
+	Stembg growStem(int id,int x1, int y1, int level, int angle, int parentid,int multiplier )
+	{
+		id++;
+		Stembg s=new Stembg();
+		s.id=id;
+		s.x1=x1;
+		s.y1=y1;
+		s.level=level;
+		s.parentid=parentid;
+		// Calculating the end points
+		s.x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * (10-level) *multiplier);
+		s.y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) *(10-level) * multiplier);
+		s.length=(int)Math.sqrt((s.y2-s.y1)*(s.y2-s.y1)+(s.x2-s.x1)*(s.x2-s.x1));// to find the length of each stem
+		s.angle=angle;
+		//System.out.println(toStringStem(s));
+		return s;
+		
+	}
+
+}
---
+++ PlantProject/src/edu/neu/csye6200/bg/StemList.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.neu.csye6200.bg;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author krishnamodi
+ *
+ */
+public class StemList {
+
+	static ArrayList<Stembg> slist2=new ArrayList<>(1090);
+	
+	ArrayList <ArrayList<Stembg>> sl;
+	
+	void add(Collection<? extends StemList> stem) {
+	//	sl.addAll(stem);
+		
+		
+	}
+}
